4aea0761b6c7e34ae7be3303ee8054cf7e169238
Kenan Sulayman <Kenan@Sulayman.org>
2013-01-20 12:37:26 +0100

    Thumb Matrix Experiment

---
+++ examples/thumb_matrix.js
@@ -0,0 +1,37 @@
+require("../lib/ps3.js");
+
+util = this['util'] || require("util");
+
+log = function () {
+        return process.stdout.write( "\r" + util.format.apply(this, arguments));
+}
+
+// thumb matrix
+
+// setInterval(function () {
+//         rpacket = [
+//                 rdata[6],
+//                 rdata[7],
+//                 rdata[8],
+//                 rdata[9],
+//         ];
+//         log( rpacket.join("--")+"--")
+// }, 20)
+
+// projection: 255 x 255 => 1920 x 1080
+
+
+setInterval(function () {
+        ld = [ 1920, 1080 ]
+        
+        rpacket = [
+                rdata[6] * ld[0], // x 1920
+                rdata[7] * ld[1], // x 1080
+                rdata[8] * ld[0], // x 1920
+                rdata[9] * ld[1], // x 1080
+        ].map(function (v) {
+                return v / 255
+        });
+
+        console.log( rpacket )
+}, 20)
